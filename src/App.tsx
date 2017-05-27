f5f680c9f855a7fdbef6ee9ba7a7f23e1299c751
j- <j@skeoh.com>
2017-05-27 18:38:50 +1000

    Implement basic file type checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,57 @@
 import * as React from 'react';
 import './App.css';
+import { Observable, Subscription } from 'rxjs';
+import * as fileType from 'file-type';
 
-const logo = require('./logo.svg');
+const { fromEvent, merge } = Observable;
+
+// Event streams
+const pasteEvents = fromEvent<ClipboardEvent>(window, 'paste');
+const dragEvents = fromEvent<DragEvent>(window, 'dragover');
+const dropEvents = fromEvent<DragEvent>(window, 'drop');
+const dragAndDropEvents = merge(dragEvents, dropEvents);
+
+// Get transfer data from events
+const dropData = dropEvents.pluck<DragEvent, DataTransfer>('dataTransfer');
+const pasteData = pasteEvents.pluck<ClipboardEvent, DataTransfer>('clipboardData');
+const dataTransfers = merge(dropData, pasteData);
+
+// Grab files from each data transfer
+const files = dataTransfers.flatMap((dataTransfer) => dataTransfer.files);
+
+// Get array buffer from each file
+const buffers = files.flatMap((file) => (
+	new Promise<Buffer>((resolve) => {
+		const reader = new FileReader();
+		reader.addEventListener('load', () => resolve(reader.result));
+		reader.readAsArrayBuffer(file);
+	})
+));
+
+// Get file information from each buffer
+const fileTypes = buffers.map((result) => fileType(result));
 
 class App extends React.Component<{}, null> {
+	private dragDropEventSubscription: Subscription;
+	private dataTransferSubscription: Subscription;
+
+	public componentDidMount() {
+		this.dragDropEventSubscription = dragAndDropEvents
+			.subscribe((e) => e.preventDefault());
+		this.dataTransferSubscription = fileTypes
+			.subscribe((file) => console.log(file));
+	}
+
+	public componentWillUnmount() {
+		this.dragDropEventSubscription.unsubscribe();
+		this.dataTransferSubscription.unsubscribe();
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.tsx</code> and save to reload.
-				</p>
+				<h1>File Types</h1>
+				<p>Drag+drop or copy+paste files into this window. Check console for results.</p>
 			</div>
 		);
 	}
