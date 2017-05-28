1602e074368db72f5a815b4bef0739db21e029c6
j- <j@skeoh.com>
2017-05-28 18:41:18 +1000

    Simplify root reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -5,15 +5,13 @@ import files, {
 	getFiles as getFilesFromFilesState,
 } from './files';
 
-const rootReducer = combineReducers({
-	files,
-});
-
 export type RootReducerState = {
 	files: FilesReducerState;
 };
 
-export default rootReducer;
+export default combineReducers<RootReducerState>({
+	files,
+});
 
 export const getFiles = (state: RootReducerState) => (
 	getFilesFromFilesState(state.files)
