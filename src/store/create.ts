eca18ea6536c2e941adb5123ae750759332dca9d
j- <j@skeoh.com>
2018-06-05 00:33:06 +1000

    Install redux-devtools-extension dependency

--- src/store/create.ts
+++ src/store/create.ts
@@ -1,15 +1,10 @@
 import { createStore as createReduxStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer';
-
-interface Window {
-	__REDUX_DEVTOOLS_EXTENSION__?: Function;
-}
-
-declare var window: Window;
 
 export const createStore = () => (
 	createReduxStore(
 		rootReducer,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		composeWithDevTools()
 	)
 );
